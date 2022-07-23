7b63dce8f7e9b54b877687094a0330061fde45d0
Mauro <MauroMarchesan88@gmail.com>
2022-07-22 21:53:01 -0300

    retirando format onsave para parar com o inferno do eslint

--- src/App.js
+++ src/App.js
@@ -20,15 +20,15 @@ function App() {
     <table>
       <tr>
         {Object.keys(data[0]).map(
-          (key) => (<th key={Math.random()}>{key}</th>),
+          (key) => (<th key={ Math.random() }>{key}</th>),
         )}
       </tr>
       <tbody>
         {data.map((rowInfo) => (
-          <tr key={Math.random()}>
+          <tr key={ Math.random() }>
             {
               Object.values(rowInfo).map(
-                (cellInfo) => (<td key={Math.random()}>{cellInfo}</td>),
+                (cellInfo) => (<td key={ Math.random() }>{cellInfo}</td>),
               )
             }
           </tr>
